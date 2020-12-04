f4029043b5644d7d61d26e456840aace6db36b04
suyeon <koo571792@gmail.com>
2020-12-04 19:16:51 +0900

    test Read Users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,19 @@
+const userService = require('../services/userService');
+const { User } = require('../models')
+const statusCode = require('../modules/statusCode');
+const responseBody = require('../modules/responseBody');
+
 module.exports = {
-    signUp : function(req, res) {
-        let status = 400;
-        res.json({
-            'status' : status
-        });
+    signUp : async (req, res) => {
+        try{
+            const users = await User.findAll({
+                attributes: ['id', 'email', 'name']
+            });
+            console.log(users);
+            return res.status(statusCode.OK).send(responseBody.success(statusCode.OK, '사용자 조회 성공', users));
+        } catch(err) {
+            console.error(err);
+            return res.status(statusCode.INTERNAL_SERVER_ERROR).send(responseBody.fail(statusCode.INTERNAL_SERVER_ERROR, '사용자 조회 실패'))
+        }
     }
 };
