d1dc2e86f296d32dde629bfe97826b432ad71dfa
soo5717 <koo5717@naver.com>
2020-12-06 20:27:37 +0900

    error

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,7 +11,7 @@ module.exports = {
             return res.status(sc.BAD_REQUEST).send(rb.fail(sc.BAD_REQUEST, rm.NULL_VALUE));
         }
         try{
-            await userService.create(email, pwd, name, country);
+            await userService.signUp(email, pwd, name, country);
             return res.status(sc.CREATED).send(rb.success(sc.CREATED, rm.SIGNUP_SUCCESS));
         } catch(e) {
             console.error(e);
